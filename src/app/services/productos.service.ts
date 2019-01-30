5b888e7914b15ebd720cd8132adb5276ff7e01c4
Ignacio Febles <ifebles@gmail.com>
2019-01-30 18:33:32 +0100

    Fin de la seccion 7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Producto } from '../interfaces/producto.interfase';
+import { resolve } from '../../../node_modules/@types/q';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,8 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
+
 
   constructor( private http: HttpClient ) {
 
@@ -16,15 +19,61 @@ export class ProductosService {
 
   }
 
-  private cargarProductos(){
-    this.http.get('https://angular-html-ba7e1.firebaseio.com/productos_idx.json')
-      .subscribe( ( resp: Producto[] ) =>{
 
-          console.log(resp);
+  private cargarProductos(){
+
+    return new Promise( ( resolve, reject ) => {
+
+      this.http.get('https://angular-html-ba7e1.firebaseio.com/productos_idx.json')
+        .subscribe( (resp: Producto[]) => {
           this.productos = resp;
           this.cargando = false;
+          resolve();
+      });
 
-      } )
+    })
+
   }
 
+  getProducto( id: string ) {
+
+    return this.http.get(`https://angular-html-ba7e1.firebaseio.com/productos/${ id  }.json`)
+
+  }
+
+  buscarProducto( termino: string ) {
+
+
+    if ( this.productos.length === 0 ) {
+      // cargar Productos
+      this.cargarProductos().then( () => {
+        // ejecutar despues de tener los Productos
+        // aplicar filtro
+        this.filtrarProductos( termino );
+      });
+    } else {
+      // aplicar filtro
+      this.filtrarProductos( termino );
+    }
+
+  }
+
+  private filtrarProductos( termino: string ) {
+
+    // console.log(this.productos);
+    this.productosFiltrado = [];
+
+    termino = termino.toLocaleLowerCase();
+
+    this.productos.forEach( prod => {
+
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+
+      if( prod.categoria.indexOf( termino ) >= 0 || tituloLower.indexOf( termino ) >= 0 ) {
+        this.productosFiltrado.push( prod );
+      }
+
+    });
+
+  }
 }
